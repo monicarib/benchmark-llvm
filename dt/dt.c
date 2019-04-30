d9fde462b8500a2556acebab4f4a3f95518b03f7
Mônica Ribeiro <monicarib1@gmail.com>
2019-04-30 11:54:28 -0300

    DT Algorithm is working executing the test with leon3, but not with larad. Made by Professor Gustavo.

--- dt/dt.c
+++ dt/dt.c
@@ -1,14 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include "dt.h"
 
-enum {
-  ITERATIONS = 131072,
-  size = 2048
-};
-
-static inline void
-dt(double * __restrict dvec1, double * __restrict dvec2){
+//static inline void dt(double * __restrict dvec1, double * __restrict dvec2, int ITERATIONS, int size){
+void dt(double * __restrict dvec1, double * __restrict dvec2, int ITERATIONS, int size){
   long i, j;
   for(j = 0; j < ITERATIONS; j++)
     for(i = 0; i < size; i++)
--- dt/dt.h
+++ dt/dt.h
@@ -1,1 +1,1 @@
-dt(double * __restrict dvec1, double * __restrict dvec2)
+void dt(double * __restrict dvec1, double * __restrict dvec2, int ITERATIONS, int size);
