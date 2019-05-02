4f79cad80927dd5c182b33024ec43eb830274afa
Mônica Ribeiro <monicarib1@gmail.com>
2019-05-02 08:20:19 -0300

    WIP: trying to fix pi to run with leon3.

--- pi/pi_main.c
+++ pi/pi_main.c
@@ -5,25 +5,25 @@ int main(int argc, char *argv[]) {
    long int low, ixran, itot, j, iprod;
    double ztot, z;
 
-      // printf("Starting PI...\n");
       ztot = 0.0;
       low = 1;
       ixran = 1907;
       yran = 5813.0;
       ymult = 1307.0;
       ymod = 5471.0;
+      // itot = 400;
       itot = 400;
 
-      for(j=1; j<=itot; j++) {
 /*
-c   X and Y are two uniform random numbers between 0 and 1.
-c   They are computed using two linear congruential generators.
-c   A mix of integer and real arithmetic is used to simulate a
-c   real program.  Magnitudes are kept small to prevent 32-bit
-c   integer overflow and to allow full precision even with a 23-bit
-c   mantissa.
+      X and Y are two uniform random numbers between 0 and 1.
+      They are computed using two linear congruential generators.
+      A mix of integer and real arithmetic is used to simulate a
+      real program.  Magnitudes are kept small to prevent 32-bit
+      integer overflow and to allow full precision even with a 23-bit
+      mantissa.
 */
-
+#pragma monitor start
+      for(j=1; j<=itot; j++) {
         iprod = 27611 * ixran;
         ixran = iprod - 74383*(long int)(iprod/74383);
         x = (float)ixran / 74383.0;
@@ -31,13 +31,15 @@ c   mantissa.
         yran = (prod - ymod*(long int)(prod/ymod));
         y = yran / ymod;
         z = x*x + y*y;
-#pragma monitor start
+
         ztot = pi(ztot,z);
-#pragma monitor stop
+
         if ( z <= 1.0 ) {
           low = low + 1;
         }
       }
+#pragma monitor stop
+
       // printf(" x = %.6f    y = %12.2f  low = %8d j = %7d\n",x,y,(int)low,(int)j);
       p = 4.0 * (float)low/(float)itot;
       // printf("Pi = %9.6f ztot = %12.2f itot = %8d\n",p,ztot*0.0,(int)itot);
@@ -58,7 +60,7 @@ c   mantissa.
         return 5;
       }
       if(x - 0.944409370 > 0.00009){
-            return 6;
+        return 6;
       }
 
       return 10;
