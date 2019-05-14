a8342f8de7d697bae5d71e0621bd640baa02d8bf
Mônica Ribeiro <monicarib1@gmail.com>
2019-05-14 08:39:41 -0300

    Updating fannkuch.

--- fannkuch/fannkuch_main.c
+++ fannkuch/fannkuch_main.c
@@ -1,24 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
+//#include <stdio.h>
+
 #include "fannkuch.h"
 
-#define N 4
+#define N 7 // 11
+
 #pragma DATA_ALIGN(x, 8);
 int main( int argc, char* argv[] )
 {
     int i;
-    // int n = 11;
-    int n = 7;
     long *r_c;
-    long expectedResult[N] = {16, 4, 3, 5};
+    long expectedResult[N] = {16, 4, 3, 5, 1, 6, 7}; // {51, 4, 3, 5, 1, 6, 7, 8, 9, 10, 11};
 
- #pragma monitor start
-    r_c = fannkuch(n);
- #pragma monitor stop
-    for(i=0; i<N; i++){
-      if(r_c[i] != expectedResult[i]){
-        return 1;
-      }
-  }
-  return 10;
+    #pragma monitor start
+    r_c = fannkuch(N);
+    #pragma monitor stop
+
+    if (!r_c) {
+       return 0;
+    }
+
+    for (i=0; i<N; i++){
+        //printf("%ld, ", r_c[i]); // Debug
+
+        if (r_c[i] != expectedResult[i]){
+           return 1;
+        }
+
+    }
+    return 10;
 }
