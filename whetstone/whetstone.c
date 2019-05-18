bc891439d839c3adba649580f524650fbd1e9b6b
Mônica Ribeiro <monicarib1@gmail.com>
2019-05-18 12:17:16 -0300

    Fixing whetstone.

--- whetstone/whetstone.c
+++ whetstone/whetstone.c
@@ -1,64 +1,6 @@
 #define PRINTOUT
-/*
- * C Converted Whetstone Double Precision Benchmark
- *		Version 1.2	22 March 1998
- *
- *	(c) Copyright 1998 Painter Engineering, Inc.
- *		All Rights Reserved.
- *
- *		Permission is granted to use, duplicate, and
- *		publish this text and program as long as it
- *		includes this entire comment block and limited
- *		rights reference.
- *
- * Converted by Rich Painter, Painter Engineering, Inc. based on the
- * www.netlib.org benchmark/whetstoned version obtained 16 March 1998.
- *
- * A novel approach was used here to keep the look and feel of the
- * FORTRAN version.  Altering the FORTRAN-based array indices,
- * starting at element 1, to start at element 0 for C, would require
- * numerous changes, including decrementing the variable indices by 1.
- * Instead, the array E1[] was declared 1 element larger in C.  This
- * allows the FORTRAN index range to function without any literal or
- * variable indices changes.  The array element E1[0] is simply never
- * used and does not alter the benchmark results.
- *
- * The major FORTRAN comment blocks were retained to minimize
- * differences between versions.  Modules N5 and N12, like in the
- * FORTRAN version, have been eliminated here.
- *
- * An optional command-line argument has been provided [-c] to
- * offer continuous repetition of the entire benchmark.
- * An optional argument for setting an alternate LOOP count is also
- * provided.  Define PRINTOUT to cause the POUT() function to print
- * outputs at various stages.  Final timing measurements should be
- * made with the PRINTOUT undefined.
- *
- * Questions and comments may be directed to the author at
- *			r.painter@ieee.org
- */
-/*
-C**********************************************************************
-C     Benchmark #2 -- Double  Precision Whetstone (A001)
-C
-C     o	This is a REAL*8 version of
-C	the Whetstone benchmark program.
-C
-C     o	DO-loop semantics are ANSI-66 compatible.
-C
-C     o	Final measurements are to be made with all
-C	WRITE statements and FORMAT sttements removed.
-C
-C**********************************************************************
-*/
 
-/* standard C library headers required */
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <math.h>
-
-/* the following is optional depending on the timing function used */
 #include <time.h>
 
 /* map the FORTRAN math functions, etc. to the C versions */
@@ -70,16 +12,11 @@ C**********************************************************************
 #define DSQRT	sqrt
 #define IF		if
 
-/* function prototypes */
-#define USAGE	"usage: whetdc [-c] [loops]\n"
-
 /*
 	COMMON T,T1,T2,E1(4),J,K,L
 */
 double T,T1,T2,E1[5];
 int J,K,L;
-
-
 
 void
 PA(double E[])
@@ -127,36 +64,7 @@ POUT(long N, long J, long K, double X1, double X2, double X3, double X4)
 }
 #endif
 
-int whetstone(int argc, char *argv[]){
-	/* used in the FORTRAN version */
-	long I;
-	long N1, N2, N3, N4, N6, N7, N8, N9, N10, N11;
-	double X1,X2,X3,X4,X,Y,Z;
-	long LOOP;
-	int II, JJ;
-	long resultado;
-
-	/* added for this version */
-	long loopstart;
-	long startsec, finisec;
-	float KIPS;
-	int continuous;
-
-	loopstart = 250;
-	continuous = 0;
-
-	II = 1;		/* start at the first arg (temp use of II here) */
-	while (II < argc) {
-		if (strncmp(argv[II], "-c", 2) == 0 || argv[II][0] == 'c') {
-			continuous = 1;
-		} else if (atol(argv[II]) > 0) {
-			loopstart = atol(argv[II]);
-		} else {
-			// fprintf(stderr, USAGE);
-			return(1);
-		}
-		II++;
-	}
+int whetstone(long startsec, long LOOP, long loopstart, int II, int JJ, long N1, long N2, long N3, long N4, long N6, long N7, long N8, long N9, long N10, long N11, long X1, long X2, long X3, long X4, long I, long resultado, int continuous, double X, double Y, double Z, long finisec) {
 
 LCONT:
 /*
@@ -223,6 +131,7 @@ C
 C	Module 2: Array elements
 C
 */
+
 	E1[1] =  1.0;
 	E1[2] = -1.0;
 	E1[3] = -1.0;
@@ -236,7 +145,9 @@ C
 	}
 
 #ifdef PRINTOUT
+
 	IF (JJ==II) resultado = POUT(N2,N3,N2,E1[1],E1[2],E1[3],E1[4]);
+
 #endif
 
 /*
@@ -248,7 +159,9 @@ C
 		PA(E1);
 
 #ifdef PRINTOUT
+
 	IF (JJ==II) resultado = POUT(N3,N2,N2,E1[1],E1[2],E1[3],E1[4]);
+
 #endif
 
 /*
@@ -256,6 +169,7 @@ C
 C	Module 4: Conditional jumps
 C
 */
+
 	J = 1;
 	for (I = 1; I <= N4; I++) {
 		if (J == 1)
@@ -426,11 +340,9 @@ C--------------------------------------------------------------------
 	else
 		printf("C Converted Double Precision Whetstones: %.1f KIPS\n", KIPS);
 #endif
-	if (continuous)
+	if (continuous) {
 		goto LCONT;
+	}
 
-    if (resultado - (long)9300000 > 0)
-        return 1;
-
-	return(10);
+    return resultado;
 }
--- whetstone/whetstone_main.c
+++ whetstone/whetstone_main.c
@@ -2,9 +2,30 @@
 
 int main(int argc, char *argv[])
 {
-    int resultado = whetstone(argc, argv);
-    if (resultado - (long)9300000 > 0)
-        return 1;
+	long I;
+	long N1, N2, N3, N4, N6, N7, N8, N9, N10, N11;
+	double X1,X2,X3,X4,X,Y,Z;
+	long LOOP;
+	int II, JJ;
+	long resultado;
 
-	return(10);
+	long loopstart;
+	long startsec, finisec;
+	float KIPS;
+	int continuous;
+
+	loopstart = 52; // 250
+	continuous = 0;
+
+	II = 1;
+
+#pragma monitor start
+    int result = whetstone(startsec, LOOP, loopstart, II, JJ, N1, N2, N3, N4, N6, N7, N8, N9, N10, N11, X1, X2, X3, X4, I, resultado, continuous, X, Y, Z, finisec);
+#pragma monitor stop
+
+    if (result - (long)9300000 > 0) {
+        return 1;
+    }
+        
+	return 10;
 }
